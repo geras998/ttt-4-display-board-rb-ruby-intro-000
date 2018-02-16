89fa92bacadb5d2428b8a0c05653205b62c05a55
Gerardo Dueñas <geras998@gmail.com>
2018-02-16 00:31:00 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,8 +1,11 @@
 # Define display_board that accepts a board and prints
 # out the current state.
 def display_board(board)
-  board.lenght.times do |i|
+  3.times do |i|
     3.times do |j|
+      puts board[i]
     end
+    puts ""
+    puts "-" * 11
   end
 end
