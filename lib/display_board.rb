33d6fae16df554548b5806bd66fbf7d1c845dba1
Gerardo Dueñas <geras998@gmail.com>
2018-02-16 00:50:21 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -3,9 +3,6 @@
 def display_board(board)
   9.times do |i|
     print " #{board[i]} "
-    if (i+1) % 3 != 0 then
-      puts "|"
-    end
     if (i+1) % 3 == 0 and i != 0 then
       puts ""
       puts "-" * 11
