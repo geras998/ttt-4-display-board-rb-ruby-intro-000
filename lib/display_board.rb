b6541989fab4228c7270f7d11bd9e50cbceee84c
Gerardo Dueñas <geras998@gmail.com>
2018-02-16 00:53:33 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -6,9 +6,8 @@ def display_board(board)
     if (i+1) % 3 == 0 and i != 8 then
       puts ""
       puts "-" * 11
-    else
-      print "|"
     end
+    print "|" if (i+1) % 3 != 0
   end
   puts ""
 end
