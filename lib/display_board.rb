51768b026b6acee36f43cedc45e17341afa892e9
Gerardo Dueñas <geras998@gmail.com>
2018-02-16 00:41:36 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -3,7 +3,7 @@
 def display_board(board)
   3.times do |i|
     3.times do |j|
-      print " #{board[i * (j+1) + j]} "
+      print " #{board[]} "
       if j != 2 then
         print "|"
       end
