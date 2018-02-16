c1a3d14ccf815d9e2037f3c1b9ef99e509d3064a
Gerardo Dueñas <geras998@gmail.com>
2018-02-16 00:37:52 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,8 +1,7 @@
 # Define display_board that accepts a board and prints
 # out the current state.
 def display_board(board)
-  3.times do |i|
-    3.times do |j|
+  9.times do |i|
       print " #{board[i * j + j]} "
       if j != 2 then
         print "|"
